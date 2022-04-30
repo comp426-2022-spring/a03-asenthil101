761561f45ffd79400b0ccf2d8b900475a85a750e
Ashwin Senthil <asenthil@email.unc.edu>
2022-04-30 17:10:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,59 @@ const HTTP_PORT = args.port || 5000;
 const server = app.listen(HTTP_PORT, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
 });
-
+export function coinFlip() {
+    var randomNum = Math.random()
+    if (randomNum > 0.5) {
+      return "heads"
+    }
+    else {
+      return "tails"
+    }
+  }
+export function coinFlips(flips) { 
+    let flipList = []; 
+    let i = 0; 
+    for (let i=0; i < flips; i++) {
+        flipList.push(coinFlip());
+    }
+    return flipList;
+}
+export function countFlips(array) {
+    var count;
+    var heads = 0;
+    var tails = 0;
+    var i = 0;
+    for (let i=0; i < array.length; i++) {
+        if (array[i] === "tails") {
+        tails += 1;
+        } else {
+        heads += 1;
+        }
+}
+    if (heads == 0) {
+        count = { tails };
+    } else if (tails == 0) {
+        count = { heads };
+    } else {
+        count = { tails, heads }; 
+    }
+    return count;
+}
+export function flipACoin(call) {
+    var statement = {
+      call,
+      flip: coinFlip(),
+      result: " ",
+    }; 
+  
+    if (statement.call == statement.flip) {
+      statement.result = "win";
+    } 
+    else {
+      statement.result = "lose";
+    } 
+    return statement; 
+  }
 // Default response for any other request
 app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
