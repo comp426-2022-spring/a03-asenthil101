00327e2113fdf3f86484baeb8c7f5acad561befd
Ashwin Senthil <asenthil@email.unc.edu>
2022-04-30 18:02:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const args = require('minimist')(process.argv.slice(2))
 const port = args.port || 5000;
 // Start an app server
 const server = app.listen(port, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%',port))
+    console.group('App listening on port %PORT%'.replace('%PORT%',port))
 });
 function coinFlip() {
     var randomNum = Math.random()
@@ -62,10 +62,6 @@ function flipACoin(call) {
 // Default response for any other request
 app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
-});
-app.use(function(req,res) {
-    res.status(404).end('Endpoint does not exist');
-    res.type('text/plain');
 });
 app.get('/app/', (req, res) => {
     // Respond with status 200
