eb7367dfc9459caf241ab3545e28febb24ed9dfb
Ashwin Senthil <asenthil@email.unc.edu>
2022-04-30 17:07:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
 const express = require('express')
 const app = express
+const args = require('minimst')(process.argv.slice(2))
+const HTTP_PORT = args.port || 5000;
+// Start an app server
+const server = app.listen(HTTP_PORT, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
+});
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND')
+});
+
+app.get('/app/', (req, res) => {
+    // Respond with status 200
+        res.statusCode = 200;
+    // Respond with status message "OK"
+        res.statusMessage = 'OK';
+        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+        res.end(res.statusCode+ ' ' +res.statusMessage)
+    });
